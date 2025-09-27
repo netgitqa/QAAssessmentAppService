0e1795aa2d7a82c7ffb2fadab4a646dda570327b
netgitqa <netgitqa@gmail.com>
2025-09-27 14:18:09 +0200

    change test n

--- specs/login.spec.js
+++ specs/login.spec.js
@@ -7,6 +7,7 @@ const EMAIL = process.env.EMAIL_VALUE;
 const PASSWORD = process.env.PASSWORD_VALUE;
 const FAKE_EMAIL = 'fakevalue@test.com';
 const FAKE_PASSWORD = 'fakevalue';
+const STORAGE_STATE_PATH = process.env.STORAGE_STATE_PATH;
 
 test.describe('User Authentication', () => {
   let login;
