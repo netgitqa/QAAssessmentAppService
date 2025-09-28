ac3f3de8a142e005b311e1c399b2af4528cbe73c
netgitqa <netgitqa@gmail.com>
2025-09-28 08:18:03 +0200

    change test n

--- conf/webkit-mac.config.js
+++ conf/webkit-mac.config.js
@@ -3,28 +3,27 @@ const path = require('path');
 const commonConfig = require('./common.config');
 
 const capabilities = {
-    browserName: 'pw-webkit',
-    browserVersion: '18.0',
-    'LT:Options': {
-        platform: 'macOS Big sur',
-        build: 'Login Test',
-        name: 'Playwright Webkit Big sur',
-        user: process.env.LT_USERNAME,
-        accessKey: process.env.LT_ACCESS_KEY,
-        network: true,
-        video: true,
-        console: true,
-
-    }
+  browserName: 'pw-webkit',
+  browserVersion: '18.0',
+  'LT:Options': {
+    platform: 'macOS Big sur',
+    build: 'Login Test',
+    name: 'Playwright Webkit Big sur',
+    user: process.env.LT_USERNAME,
+    accessKey: process.env.LT_ACCESS_KEY,
+    network: true,
+    video: true,
+    console: true,
+  }
 };
 
 module.exports = defineConfig({
-    ...commonConfig,
-    use: {
-        ...commonConfig.use,
-        storageState: path.resolve(__dirname, '../storageState.json'),
-        connectOptions: {
-            wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
-        }
+  ...commonConfig,
+  use: {
+    ...commonConfig.use,
+    storageState: path.resolve(__dirname, '../storageState.json'),
+    connectOptions: {
+      wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
     }
+  }
 });
