01aea33efb283642a83e060f2dae536a911a51f0
Dmitry <ec-dmitry.sadikov@ninjavan.co>
2025-10-11 15:50:59 +0200

    change test n

--- conf/webkit-mac.config.js
+++ conf/webkit-mac.config.js
@@ -3,7 +3,7 @@ const commonConfig = require('./common.config');
 
 const capabilities = {
   browserName: 'pw-webkit',
-  browserVersion: '18.2',
+  browserVersion: '18.0',
   'LT:Options': {
     platform: 'macOS Big sur',
     build: 'Login Test',
