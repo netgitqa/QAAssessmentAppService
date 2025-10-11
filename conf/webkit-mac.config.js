3a47e8fbc13018392c8f44d10298aa3f0b9bf6c1
Dmitry <ec-dmitry.sadikov@ninjavan.co>
2025-10-11 16:36:17 +0200

    change test n

--- conf/webkit-mac.config.js
+++ conf/webkit-mac.config.js
@@ -1,27 +1,34 @@
-const { defineConfig } = require('@playwright/test');
-const commonConfig = require('./common.config');
-
-const capabilities = {
-  browserName: 'pw-webkit',
-  browserVersion: '18.0',
-  'LT:Options': {
-    platform: 'macOS Big sur',
-    build: 'Login Test',
-    name: 'Playwright Webkit Big sur',
-    user: process.env.LT_USERNAME,
-    accessKey: process.env.LT_ACCESS_KEY,
-    network: true,
-    video: true,
-    console: true,
-  }
-};
+const { defineConfig, devices } = require('@playwright/test');
+const commonConfig = require('./common.conf');
 
 module.exports = defineConfig({
   ...commonConfig,
-  use: {
-    ...commonConfig.use,
-    connectOptions: {
-      wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
-    }
-  }
+  projects: [
+    {
+      name: 'WebKit macOS Big Sur',
+      use: {
+        ...devices['Desktop Safari'],
+        browserName: 'pw-webkit',
+        browserVersion: '18.0',
+        connectOptions: {
+          wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+              JSON.stringify({
+                browserName: 'pw-webkit',
+                browserVersion: '18.0',
+                'LT:Options': {
+                  platform: 'macOS Big Sur',
+                  build: 'Login Test',
+                  name: 'Playwright WebKit Big Sur',
+                  user: process.env.LT_USERNAME,
+                  accessKey: process.env.LT_ACCESS_KEY,
+                  network: true,
+                  video: true,
+                  console: true,
+                },
+              })
+          )}`,
+        },
+      },
+    },
+  ],
 });
