dd1441292e8489b696b7270fcfce5369d7085712
Dmitry <ec-dmitry.sadikov@ninjavan.co>
2025-10-12 16:27:56 +0200

    change test n

--- pageobjects/loginPage.js
+++ pageobjects/loginPage.js
@@ -1,5 +1,4 @@
-import * as allure from 'allure-js-commons';
-import Logger from '../utils/logger';
+import * as allureReporter from 'allure-js-commons';
 
 class LoginPage {
   constructor(page) {
@@ -13,43 +12,47 @@ class LoginPage {
   }
 
   async open() {
-    await allure.step('Navigating to login page', async () => {
+    await allureReporter.step('Navigating to login page', async () => {
       await this.page.goto('/login');
     });
   }
 
   async enterEmail(email) {
-    await allure.step(`Entering email: ${email}`, async () => {
+    await allureReporter.step(`Entering email: ${email}`, async () => {
       await this.page.fill(this.emailInput, email);
     });
   }
 
   async enterPassword(password) {
-    Logger.step(`Entering password`);
-    await this.page.fill(this.passwordInput, password);
+    await allureReporter.step(`Entering password`, async () => {
+      await this.page.fill(this.passwordInput, password);
+    });
   }
 
   async submitLogin() {
-    await Logger.step('Clicking login button');
-    await this.page.click(this.loginButton);
+    await allureReporter.step('Clicking login button', async () => {
+      await this.page.click(this.loginButton);
+    });
   }
     
   async clickForgotPassword() {
-    await Logger.step('Clicking forgot password link');
-    await this.page.click(this.forgotPasswordLink);
+    await allureReporter.step('Clicking forgot password link', async () => {
+      await this.page.click(this.forgotPasswordLink);
+    });
   }
 
   async getErrorMessages() {
-    await Logger.step('Getting error messages');
-    await this.page.waitForSelector(this.errorMessages, {
-      timeout: 5000,
-      state: 'visible'
-    });
+    await allureReporter.step('Getting error messages', async () => {
+      await this.page.waitForSelector(this.errorMessages, {
+        timeout: 5000,
+        state: 'visible'
+      });
 
-    const errors = await this.page.$$eval(this.errorMessages, elements =>
-      elements.map(el => el.textContent.trim()).join(' ')
-    );
-    return errors;
+      const errors = await this.page.$$eval(this.errorMessages, elements =>
+        elements.map(el => el.textContent.trim()).join(' ')
+      );
+      return errors;
+    });
   }
 }
 
