20c6198063cc4715ce8df878d03caa4ae4ad5358
Dmitry <ec-dmitry.sadikov@ninjavan.co>
2025-10-12 20:55:07 +0200

    change test n

--- conf/common.config.js
+++ conf/common.config.js
@@ -23,6 +23,7 @@ const getCapabilities = (browserName, platform, buildName, testName, specName) =
 
 module.exports = defineConfig({
     testDir: path.resolve(__dirname, '../specs'),
+    testMatch: ['**/*.spec.js'],
     timeout: 30000,
     retries: 0,
     reporter: [['list'], ['allure-playwright']],
