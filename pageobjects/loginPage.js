44cb49710c5bc888b91b727f2082551f08a24998
Dmitry <ec-dmitry.sadikov@ninjavan.co>
2025-10-12 12:46:43 +0200

    change test n

--- pageobjects/loginPage.js
+++ pageobjects/loginPage.js
@@ -11,63 +11,55 @@ class LoginPage {
     this.errorMessages = 'p.usahello-form-error.usahello-form-validation_msg';
   }
 
-  async goto() {
-    await Logger.step('Navigating to login page', async () => {
-      await this.page.goto('/login');
-    });
+  async open() {
+    await Logger.step('Navigating to login page');
+    await this.page.goto('/login');
   }
 
   async enterEmail(email) {
-    await Logger.step(`Entering email: ${email}`, async () => {
-      await this.page.fill(this.emailInput, email);
-    });
+    await Logger.step(`Entering email: ${email}`);
+    await this.page.fill(this.emailInput, email);
   }
 
   async enterPassword(password) {
-    Logger.step(`Entering password`, async () => {
-      await this.page.fill(this.passwordInput, password);
-    });
+    Logger.step(`Entering password`);
+    await this.page.fill(this.passwordInput, password);
   }
 
-
   async submitLogin() {
-    await Logger.step('Clicking login button', async () => {
-      await this.page.click(this.loginButton);
-    });
+    await Logger.step('Clicking login button');
+    await this.page.click(this.loginButton);
   }
     
   async clickForgotPassword() {
-    await Logger.step('Clicking forgot password link', async () => {
-      await this.page.click(this.forgotPasswordLink);
-    });
+    await Logger.step('Clicking forgot password link');
+    await this.page.click(this.forgotPasswordLink);
   }
 
   async getErrorMessages() {
-    await Logger.step('Getting error messages', async () => {
-      await this.page.waitForSelector(this.errorMessages, {
-        timeout: 5000,
-        state: 'visible'
-      });
-    
-      const errors = await this.page.$$eval(this.errorMessages, elements =>
-        elements.map(el => el.textContent.trim()).join(' ')
-      );
-      return errors;
+    await Logger.step('Getting error messages');
+    await this.page.waitForSelector(this.errorMessages, {
+      timeout: 5000,
+      state: 'visible'
     });
+
+    const errors = await this.page.$$eval(this.errorMessages, elements =>
+      elements.map(el => el.textContent.trim()).join(' ')
+    );
+    return errors;
   }
 
   async refresh() {
-    await Logger.step('Refreshing app state', async () => {
-        await this.page.waitForTimeout(500);
-        await this.page.context().clearCookies();
-        await this.page.waitForTimeout(500);
-        await this.page.evaluate(() => {
-            localStorage.clear();
-            sessionStorage.clear();
-        })
-        await this.page.waitForTimeout(500);
-        await this.page.reload();
-    });
+    await Logger.step('Refreshing app state');
+      await this.page.waitForTimeout(500);
+      await this.page.context().clearCookies();
+      await this.page.waitForTimeout(500);
+      await this.page.evaluate(() => {
+        localStorage.clear();
+        sessionStorage.clear();
+       })
+       await this.page.waitForTimeout(500);
+       await this.page.reload();
   }
 }
 
--- specs/login.js
+++ specs/login.js
@@ -22,10 +22,10 @@ test.describe('User Authentication', () => {
 
     login = new LoginPage(page);
     learning = new LearningPage(page);
-    await login.goto();
   });
 
   test('should log in with valid credentials', async ({ page }) => {
+    await login.open();
     await login.enterEmail(EMAIL);
     await login.enterPassword(PASSWORD);
     await login.submitLogin();
@@ -36,6 +36,8 @@ test.describe('User Authentication', () => {
   });
 
   test('should not log in with incorrect email', async ({ page }) => {
+    await login.refresh();
+    await login.open();
     await login.enterEmail(FAKE_EMAIL);
     await login.enterPassword(PASSWORD);
     await login.submitLogin();
@@ -45,6 +47,8 @@ test.describe('User Authentication', () => {
   });
 
   test('should not log in with incorrect password', async () => {
+    await login.refresh();
+    await login.open();
     await login.enterEmail(EMAIL);
     await login.enterPassword(FAKE_PASSWORD);
     await login.submitLogin();
@@ -54,6 +58,8 @@ test.describe('User Authentication', () => {
   });
 
   test('should not login with empty credentials', async () => {
+    await login.refresh();
+    await login.open();
     await login.enterEmail('');
     await login.enterPassword('');
     await login.submitLogin();
@@ -64,6 +70,8 @@ test.describe('User Authentication', () => {
   });
 
   test('should redirect to reset password page after clicking the Forgot password', async ({ page }) => {
+    await login.refresh();
+    await login.open();
     await login.clickForgotPassword();
     await expect(page).toHaveURL(/reset-password/);
   });
